3fe48ce4fa05c228d21d3e0b21304e8f18282df2
Alvin Joy <89687635+alvinsjoy@users.noreply.github.com>
2023-10-05 11:53:04 +0530

    Update fibonacci.java

--- fibonacci.java
+++ fibonacci.java
@@ -3,14 +3,14 @@ class fibonacci
 	public static void main(String args[])
 	{
 		long num1=0,num2=1;
-		int n=10,i=0
-        	while (i<n) 
-        	{
-            		System.out.print(num1+" ");
-            		long num3=num2+num1;
-           		num1=num2;
-           		num2=num3;
-            		i+=1;
-        	}
+		int n=10,i=0;
+        while (i<n) 
+        {
+            System.out.print(num1+" ");
+            long num3=num2+num1;
+            num1=num2;
+            num2=num3;
+            i+=1;
+        }
 	}
 }
