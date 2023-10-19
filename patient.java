d2525b41d5da863aeab0da0a899df99c2759d061
Alvin Joy <89687635+alvinsjoy@users.noreply.github.com>
2023-10-19 10:21:12 +0530

    Update patient.java

--- patient.java
+++ patient.java
@@ -1,54 +1,31 @@
-class BloodData
-{
-    private String bloodType;
-    private char rhFactor;
-    public BloodData()
-    {
-        this.bloodType = "O";
-        this.rhFactor = '+';
-    }
-    public BloodData(String bloodType, char rhFactor)
-    {
-        this.bloodType = bloodType;
-        this.rhFactor = rhFactor;
-    }
-    String getBloodType()
-    {
-        return bloodType;
-    }
-
-    char getRhFactor()
-    {
-        return rhFactor;
-    }
-}
-
+import java.util.Scanner;
 class Patient
 {
     int id;
     int age;
-    BloodData bloodData;
+    double bloodData;
 
-    public Patient() {
-        this.id = 0;
-        this.age = 0;
-        this.bloodData = new BloodData();
+    public Patient()
+    {
+        id=0;
+        age=0;
+        bloodData=0;
     }
-    public Patient(int id, int age, BloodData bloodData)
+    public Patient(int id, int age,double bloodData)
     {
         this.id = id;
         this.age = age;
-        this.bloodData = bloodData;
+        this.bloodData=bloodData;
     }
-    public int getId()
+    int getId()
     {
         return id;
     }
-    public int getAge()
+    int getAge()
     {
         return age;
     }
-    public BloodData getBloodData()
+    double getBloodData()
     {
         return bloodData;
     }
@@ -56,20 +33,23 @@ class Patient
 class TestPatient {
     public static void main(String[] args)
     {
+    	Scanner sc=new Scanner(System.in);
+    	System.out.println("Enter the patient ID: ");
+    	int id=sc.nextInt();
+    	System.out.println("Enter the patient Age: ");
+    	int age=sc.nextInt();
+    	System.out.println("Enter the patient Blood Data: ");
+    	double bloodData2=sc.nextDouble();
         Patient patient1 = new Patient();
         System.out.println("Patient 1 Default:");
         System.out.println("ID: " + patient1.getId());
         System.out.println("Age: " + patient1.getAge());
-        System.out.println("Blood Type: " + patient1.getBloodData().getBloodType());
-        System.out.println("Rh Factor: " + patient1.getBloodData().getRhFactor());
-        System.out.println();
-
-        BloodData bloodData2 = new BloodData("A", '+');
-        Patient patient2 = new Patient(12345, 30, bloodData2);
+        System.out.println("Blood Data: " + patient1.getBloodData());
+        
+        Patient patient2 = new Patient(id,age,bloodData2);
         System.out.println("Patient 2 Overloaded:");
         System.out.println("ID: " + patient2.getId());
         System.out.println("Age: " + patient2.getAge());
-        System.out.println("Blood Type: " + patient2.getBloodData().getBloodType());
-        System.out.println("Rh Factor: " + patient2.getBloodData().getRhFactor());
+        System.out.println("Blood Data: " + patient2.getBloodData());
     }
 }
