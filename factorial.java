fc34e0d94f313cb6e26c8fd3d1d2442b99fe3c26
Alvin Joy <89687635+alvinsjoy@users.noreply.github.com>
2023-10-19 11:56:38 +0530

    Add files via upload

---
+++ factorial.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class Fact
+{
+	static int factorial(int n)
+	{
+		if (n==0||n==1)
+			return 1;
+		return n*factorial(n-1);
+	}
+	public static void main(String args[])
+	{
+		Fact f=new Fact();
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the number: ");
+		int num=sc.nextInt();
+		int ans=f.factorial(num);
+		System.out.println("Factorial of "+num+" is:
+		"+ans);
+	}
+}
