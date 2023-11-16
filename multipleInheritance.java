00a68b8c7cbbea1bdad6baea52b02197d1221e22
Alvin Joy <89687635+alvinsjoy@users.noreply.github.com>
2023-11-16 11:21:25 +0530

    Update multipleInheritance.java

--- multipleInheritance.java
+++ multipleInheritance.java
@@ -1,59 +1,54 @@
-// Define the 'Sports' interface
-interface Sports {
-    float sportwt = 6.0F;
-    void putwt();
+import java.util.Scanner;
+interface Sports
+{
+	final int sportwt=6;
+	public abstract void putwt();
 }
-
-// Define the 'Student' class
-class Student {
-    int rollnumber;
-
-    void getNumber(int roll) {
-        rollnumber = roll;
-    }
-
-    void putNumber() {
-        System.out.println("Rollno:" + rollnumber);
-    }
+class Student
+{
+	Scanner sc=new Scanner(System.in);
+	int rollnumber;
+	void getNumber()
+	{
+		System.out.println("Enter the roll number:");
+		rollnumber=sc.nextInt();
+	}
+	void putNumber()
+	{
+		System.out.println("Roll number:"+rollnumber);
+	}
 }
-
-// Define the 'Test' class as a subclass of 'Student' and implements 'Sports' interface
-class Test extends Student implements Sports {
+class Test extends Student
+{
     float term1, term2;
-
-    void getMarks(float t1, float t2) {
-        term1 = t1;
-        term2 = t2;
-    }
-
-    void putMarks() {
-        System.out.println("Marks obtained");
-        System.out.println("Term 1=" + term1);
-        System.out.println("Term 2=" + term2);
-        putwt();
-        System.out.println("Total Score=" + (term1 + term2 + sportwt));
-    }
-
-    // Implement the abstract method from 'Sports' interface
-    public void putwt() {
-        System.out.println("Sports weightage=" + sportwt);
-    }
+    void getMarks()
+	{
+		System.out.println("Enter the marksfor term1:");
+		term1=sc.nextFloat();
+		System.out.println("Enter the marksfor term2:");
+		term2=sc.nextFloat();
+	}
+	void putMarks()
+	{
+		System.out.println("Term1: "+term1+"\tTerm2: "+term2);
+	}
 }
-
-// Define the 'Results' class which extends 'Test'
-class Results extends Test {
-    // This class inherits features from both 'Test' and 'Sports' (via 'Test').
-
-    public static void main(String[] args) {
-        // Create an instance of 'Results' class
-        Results result = new Results();
-
-        // Set roll number and marks
-        result.getNumber(12);
-        result.getMarks(27.5F, 33);
-
-        // Display the results
-        result.putNumber();
-        result.putMarks();
-    }
+class Results extends Test implements Sports
+{
+	public void putwt()
+	{
+		System.out.println("Enter the Sports weightage="+sportwt);
+	}
+}
+class MultInh
+{
+	public static void main(String args[])
+	{
+		Results r=new Results();
+		r.getNumber();
+		r.getMarks();
+		r.putNumber();
+		r.putMarks();
+		r.putwt();
+	}
 }
