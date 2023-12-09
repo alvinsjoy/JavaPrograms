a2f723664ea6a5f0fb9b9934207c69b8206a0c68
Alvin Joy <89687635+alvinsjoy@users.noreply.github.com>
2023-12-09 11:55:44 +0530

    Update binarySearch.java

--- binarySearch.java
+++ binarySearch.java
@@ -8,8 +8,7 @@ class Binary
 		System.out.println("Enter the array size:");
 		n=sc.nextInt();
 		int arr[]=new int[n];
-		int start=0;
-		int end=n-1;
+		int start=0,end=n-1;
 		int mid=(start+end)/2;
 		System.out.println("Enter the array elements:");
 		for(i=0;i<n;i++)
@@ -30,7 +29,7 @@ class Binary
 			}
 			else
 				end=mid-1;
-		mid=(start+end)/2;
+			mid=(start+end)/2;
 		}
 		if(start>end)
 		{
